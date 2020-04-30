ceee1c610faf9a9e56716fad321c6591f253fb1d
Andrew Harding <andrew.harding@hpe.com>
2020-04-30 16:45:32 -0600

    Add output mask to all list operations
    Signed-off-by: Andrew Harding <andrew.harding@hpe.com>

--- proto/spire/next/api/svid/v1/svid.pb.go
+++ proto/spire/next/api/svid/v1/svid.pb.go
@@ -7,7 +7,7 @@ import (
 	context "context"
 	fmt "fmt"
 	proto "github.com/golang/protobuf/proto"
-	svid "github.com/spiffe/spire/proto/spire/v2/types/svid"
+	svid "github.com/spiffe/spire/proto/spire/next/types/svid"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
